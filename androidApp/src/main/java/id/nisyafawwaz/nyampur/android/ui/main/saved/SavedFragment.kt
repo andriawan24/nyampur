c0d19a6c950d200c1a5230b6957bbafb7b8e75ca
Naufal Fawwaz <fawaznaufal23@gmail.com>
2024-08-13 22:26:17 +0700

    feature: add saved fragment

--- androidApp/src/main/java/id/nisyafawwaz/nyampur/android/ui/main/saved/SavedFragment.kt
+++ androidApp/src/main/java/id/nisyafawwaz/nyampur/android/ui/main/saved/SavedFragment.kt
@@ -1,12 +1,6 @@
 package id.nisyafawwaz.nyampur.android.ui.main.saved
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import id.nisyafawwaz.nyampur.android.base.BaseFragment
-import id.nisyafawwaz.nyampur.android.databinding.FragmentHomeBinding
 import id.nisyafawwaz.nyampur.android.databinding.FragmentSavedBinding
 
 class SavedFragment : BaseFragment<FragmentSavedBinding>() {
